aa17f4daa65dca145f02702377d0daa1ee83e44b
alexandreba <alexandre.barault@gmail.com>
2013-04-06 08:52:13 +0200

    ELMA

---
+++ SampleLog/SampleLog/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            Trace.Write("Hey, this is nice");
+            Trace.TraceWarning("Oh, this could be bad.");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+            Trace.TraceError("Oh, no! That's bad");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            Exception ex = new Exception("error on the contact action");
+            throw ex;
+            //return View();
+        }
+    }
+}
---
+++ SampleLog/SampleLog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleLog.MvcApplication" Language="C#" %>
